9f06fa2ac5de3adfd540aa4a72c93eb552e6d5d2
Silas Frantz <silas.frantz@gmail.com>
2022-01-14 13:53:07 -0500

    File upload
    Commands, shortcuts, the typical requirements.txt, and a link to downloading open source GIS binaries

---
+++ _3-7_venv_create_apyxosaurus_from_clone.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\ArcGIS\Pro\bin\Python\Scripts\conda.exe" create --name C:\PythonVenv\Apyxosaurus --clone arcgispro-py3
+pause
---
+++ _3-7_venv_install_requirements_apyxosaurus.bat
@@ -0,0 +1,2 @@
+"C:\PythonVenv\Apyxosaurus\python.exe" -m pip install -r %~dp0_3-7_venv_requirements_apyxosaurus.txt
+pause
